bc3c08a4893f4d0c09d86db9b9b29b1d5cb1afe4
Denis <krollik1000000@gmail.com>
2020-01-10 14:51:16 +0200

    react

--- lesson1/task4/src/index.js
+++ lesson1/task4/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "./index.css"
+const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const element = (
+    <div className ="greeting">
+        <div className ="greeting__title">Hello, world!</div>
+        <div className ="greeting__text">I'm learning React</div>
+    </div>
+);
+ReactDOM.render(element, rootElement);
