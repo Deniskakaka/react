5aab3026efe124d2c8a1ef93e0d54ac7bef728e1
Denis <krollik1000000@gmail.com>
2020-01-13 21:03:49 +0200

    react props

---
+++ lesson3/task4/src/Greeting.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Greeting = props => {
+   return ( 
+        <div>
+            <span>{`My name is ${'John'} ${'Doe'}. i'm ${17} years old `}</span>
+        </div>
+   );
+};
+
+export default Greeting;
---
+++ lesson3/task4/src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Greeting from './Greeting.jsx';
+
+const rootElement = document.querySelector('#root');
+
+
+
+
+ReactDOM.render(
+    <Greeting />,
+    rootElement  
+)
