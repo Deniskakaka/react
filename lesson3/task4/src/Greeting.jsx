e5a7ebd0a79b2bcdaa9151838f58b14b06ef6144
Denis <krollik1000000@gmail.com>
2020-01-13 21:05:03 +0200

    react props

--- lesson3/task4/src/Greeting.jsx
+++ lesson3/task4/src/Greeting.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Greeting = props => {
    return ( 
-        <div>
+        <div className="greeting">
             <span>{`My name is ${'John'} ${'Doe'}. i'm ${17} years old `}</span>
         </div>
    );
