9be92748efa472a42ae6f52db3a1d4b9fb3bb96f
Denis <krollik1000000@gmail.com>
2020-01-13 14:59:30 +0200

    reatc props

--- lesson3/task1/webpack.config.js
+++ lesson3/task1/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = (env, argv) => {
     const config = {
         entry: "./src/index.jsx",
         output: {
-            filename: "bundle.js"
+            filename: "review_build/bundle.js"
         },
         module: {
             rules: [
