54a698c3efc447d7e89b9ee3bd33fba6f37883a0
Denis <krollik1000000@gmail.com>
2020-01-15 14:47:37 +0200

    react events

---
+++ lesson5/task5/src/Colors.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+const RED= '#f00';
+
+class Colors extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            text : 'Off'
+        }
+    }
+
+    setBodyCOlor = (color, e) => { 
+        if (e.target.textContent === 'On') {
+            document.body.style.backgroundColor = '#ffffff'
+        }  else{
+            document.body.style.backgroundColor = color; 
+        }
+        this.changeText()
+    }
+
+   changeText = () => {
+        this.setState({
+            text: this.state.text === 'Off' ? this.state.text = 'On': this.state.text = 'Off'
+        })
+    }
+
+
+
+    render () {
+        return (
+            <div>
+                <button className="toggler"
+                    style={{backgroundColor:RED}}
+                    onClick={this.setBodyCOlor.bind(this, RED)}
+                >{this.state.text}</button>
+            </div>
+        )
+    }
+};
+
+export default Colors;
