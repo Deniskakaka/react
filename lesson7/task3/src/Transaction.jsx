fb8840b389f76ce40371ff1803ae43ee753e4cd2
Denis <krollik1000000@gmail.com>
2020-01-17 17:51:07 +0200

    react list

--- lesson7/task3/src/Transaction.jsx
+++ lesson7/task3/src/Transaction.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import moment from "moment";
 import TransactionList from './TransactionList.jsx';
 
-const formatDate = date => moment(date).format('DMMM');
-const formatTime = date => moment(date).format('h:mm');
+const formatDate = date => moment(date).format('D MMM');
+const formatTime = date => moment(date).format('HH:mm');
 
 const Transaction = (props) => {
     return <ul className="transactions">
             {props.transactions.map( i => (
-                <TransactionList  key={i.id} dateDay={formatDate(i.time)} dateTime={formatTime(i.time)} from={i.from} to={i.to} rate={i.rate} amount={i.amount}/>
+                <TransactionList  
+                    key={i.id} dateDay={formatDate(i.time)}
+                    dateTime={formatTime(i.time)} 
+                    from={i.from} to={i.to} 
+                    rate={i.rate} 
+                    amount={i.amount}   
+                 />
             ))}
            </ul>
 }
