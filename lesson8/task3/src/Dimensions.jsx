090aeba75f7289f84a6107c5a1b07ea45b352e85
Denis <krollik1000000@gmail.com>
2020-01-20 18:11:43 +0200

    react method life

---
+++ lesson8/task3/src/Dimensions.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class Dimensions extends Component {
+    state = {
+        widht: 0,
+        height: 0
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', this.onResize)
+        const {innerWidth, innerHeight} = window;
+        this.setDimensions(innerWidth, innerHeight);
+    }
+
+    componentWillMount() {
+        window.removeEventListener('resize', this.onResize)
+    }
+
+    onResize = e => {
+        const {innerWidth, innerHeight} = e.target;
+        this.setDimensions(innerWidth, innerHeight);
+    }
+
+    setDimensions = (widht, height) => {
+        this.setState({
+            widht,
+            height
+        });
+        document.title = `${this.state.widht}px - ${this.state.height}px`
+    }
+    render() {
+        return  <div className="dimensions">
+                    {`${this.state.widht}px - ${this.state.height}px`}
+                </div>
+    }
+};
+
+export default Dimensions;
