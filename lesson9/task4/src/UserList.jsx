bf094544498f344c9e9dd9fcae92eadb6a12ae26
Denis <krollik1000000@gmail.com>
2020-01-24 12:39:49 +0200

    react control form

--- lesson9/task4/src/UserList.jsx
+++ lesson9/task4/src/UserList.jsx
@@ -16,9 +16,9 @@ class UserList extends Component {
 
     render() {
         const firstLetter = this.state.value.slice(0,1).toUpperCase();
-        const otherText = this.state.value.slice(1).toLowerCase();
+        const otherLetters = this.state.value.slice(1).toLowerCase();
         const filterList = this.props.users
-                            .filter(user => user.name === (firstLetter + otherText) )
+                            .filter(user => user.name === (firstLetter + otherLetters) )
                             .map(user => <User key={user.id} name={user.name} age={user.age}/>)
         return (
             <div>
