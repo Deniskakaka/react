1a173ae7cf2dcbdc51f032565b971d26e75c83ae
Denis <krollik1000000@gmail.com>
2020-01-26 15:47:40 +0200

    react dev tools

---
+++ lesson10/task1/src/App.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import UserProfile from './UserProfile.jsx';
+import UseMenu from './UserMenu.jsx';
+
+class Page extends Component {
+    state = {
+        userData: null
+    };
+
+    componentDidMount() {
+        this.fetchUserData(this.props.userId);
+    };
+
+    fetchUserData = userId => {
+        const userUrl = `https://api.github.com/users/${userId}`;
+        fetch(userUrl)
+             .then(response => response.json())
+             .then(userData => this.setState({
+                 userData,
+             }))
+    };
+    render() {
+        return (
+            <div className="page">
+                <header className="header">
+                    <UseMenu userData={this.state.userData} />
+                </header>
+                <UserProfile userData={this.state.userData} />
+            </div>
+        )
+    }
+    
+};
+
+export default Page;
