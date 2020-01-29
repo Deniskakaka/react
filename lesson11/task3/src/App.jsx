79df7dd5804390e44e6c0f1bac0e2c79c72f570a
Denis <krollik1000000@gmail.com>
2020-01-29 12:42:44 +0200

    react

--- lesson11/task3/src/App.jsx
+++ lesson11/task3/src/App.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import Expand from './Expand.jsx';
 
 const App = () => {
-        const elements = (
-            <div className="expand__content">
-                <p>Hooks are a new addition in React 16.8. They let you use state and other React features without writing aclass.</p>
-            </div>
-        )
-        return (
-            <Expand title="Some title">
-                {elements}
-            </Expand>
-        )
+    const elements = (
+        <div className="expand__content">
+            <p>Hooks are a new addition in React 16.8. They let you use state and other React features without writing aclass.</p>
+        </div>
+    )
+    return (
+        <Expand title="Some title">
+            {elements}
+        </Expand>
+    )
 };
 
 export default App;
