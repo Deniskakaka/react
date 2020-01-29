1d730f442c97a01cb7e2026d48a4ecb87a221ed8
Denis <krollik1000000@gmail.com>
2020-01-29 16:24:03 +0200

    react todo-list

--- todo-list/src/Task.jsx
+++ todo-list/src/Task.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 
-const Task = ({ done, text }) => {
+const Task = ({ id, done, text, onChange, onDelete }) => {
     const listItemClasses = classNames('list-item', {'list-item_done': done})
     return (
         <li className={listItemClasses}>
@@ -9,9 +9,10 @@ const Task = ({ done, text }) => {
                 type="checkbox" 
                 className="list-item__checkbox"
                 defaultChecked={done}
+                onChange={() => onChange(id)}
             ></input>
                 <span className="list-item__text">{text}</span> 
-            <button className="list-item__delete-btn"></button>
+            <button className="list-item__delete-btn" onClick={() => onDelete(id)}></button>
         </li>
     )
 };
