7152b7aa98e20f383d600aae1c0f60b066b4d5b2
Denis <krollik@gmail.com>
2020-02-08 22:14:05 +0200

    react tests

---
+++ lesson13/task4/src/__test__/Dialog.test.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Dialog from '../Dialog.jsx';
+
+describe('Dialog', () => {
+    it('should isOpen equal true dialog is show', () => {
+        const wrappedComponent = shallow(<Dialog isOpen/>)
+        expect(wrappedComponent.find('.dialog').exists()).toBeTruthy();
+    });
+
+    it('should isOpen equal false dialog is none', () => {
+        const wrappedComponent = shallow(<Dialog />)
+        expect(wrappedComponent.find('.dialog').exists()).toEqual(false);
+    });
+
+    it('should onClose is function and it work', () => {
+        const mockLogin = jest.fn();
+        const wrappedComponent = shallow(<Dialog isOpen onClose={mockLogin}/>)
+        wrappedComponent.find('.dialog__close-btn').simulate('click')
+        expect(mockLogin).toBeCalled();
+    });
+
+    it('should show display elements', () => {
+        const wrappedComponent = shallow(<Dialog isOpen/>)
+        expect(wrappedComponent).toMatchSnapshot();
+    });
+
+    it('should isOpen equal false dialog is none', () => {
+        const wrappedComponent = shallow(<Dialog isOpen children={0}/>)
+        expect(wrappedComponent.find('.dialog').find('.dialog__content').text()).toEqual('0');
+    });
+})
