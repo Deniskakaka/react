adc8004e406c84ff3e8a203528302cc9ddb48a68
Denis <krollik@gmail.com>
2020-02-12 23:02:39 +0200

    react router

--- lesson14/task3/src/User.jsx
+++ lesson14/task3/src/User.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-function User({ ...rest }) {
+function User({ match }) {
   return (
     <div className="user">
       <img
         alt="User Avatar"
         src={
-          rest.match.params.userId === "facebook"
+          match.params.userId === "facebook"
             ? "https://lh3.googleusercontent.com/dkHvK43ThxbP69QBs6wNTnt_NLMmkNvr9pHiMusJnzgmAIB-KQc4AAakx3nbfHxcmw"
             : "https://avatars1.githubusercontent.com/u/9919?v=4"
         }
@@ -14,10 +14,10 @@ function User({ ...rest }) {
       />
       <div className="user__info">
         <span className="user__name">
-          {rest.match.params.userId}
+          {match.params.userId}
         </span>
         <span className="user__location">
-          {rest.match.params.userId === "facebook"
+          {match.params.userId === "facebook"
             ? "Menlo Park, CA"
             : "San Francisco,CA"}
         </span>
