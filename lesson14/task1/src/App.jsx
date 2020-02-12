2a8d65bf4f41d687a59e1148e8bfd4018acb156f
Denis <krollik@gmail.com>
2020-02-12 14:52:02 +0200

    react router

---
+++ lesson14/task1/src/App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Contacts from "./Contacts.jsx";
+import Products from "./Products.jsx";
+import Home from "./Home.jsx";
+import PageNotFound from './PageNotFound.jsx';
+import "./index.scss";
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from "react-router-dom";
+
+const App = props => {
+  return (
+    <div className="page">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/products">
+            <Products />
+          </Route>
+          <Route path="/contacts">
+            <Contacts />
+          </Route>
+          <Route path="*">
+            <PageNotFound/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
