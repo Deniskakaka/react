fdc22ecd3d9593f663eb7a3d31e6cf5731339924
Denis <krollik@gmail.com>
2020-02-13 20:45:30 +0200

    react hook

---
+++ lesson15/task2/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Users from './Users.jsx';
+import './index.scss';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<Users/>, rootElement);
