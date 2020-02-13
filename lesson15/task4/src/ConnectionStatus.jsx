1b0770cbff58adaef3825e550fa92f1e0bdd2ec3
Denis <krollik@gmail.com>
2020-02-13 22:21:42 +0200

    react hook

---
+++ lesson15/task4/src/ConnectionStatus.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState  } from 'react';
+import Offline from './Offline.jsx';
+import Online from './Online.jsx';
+
+const ConnectionStatus = () => {
+    const [Conect, setConect] = useState(true);
+
+    useEffect(() => {
+        window.addEventListener('online', () => setConect(true));
+        window.addEventListener('offline', () => setConect(false));
+
+        return () => {
+            window.removeEventListener('online', () => setConect(true));
+            window.removeEventListener('offline', () => setConect(false));
+        }
+    })
+
+    return (
+        <div className="status">
+            {Conect ? <Online/>: <Offline/>}
+        </div>
+    );
+};
+
+export default ConnectionStatus;
---
+++ lesson15/task4/src/Offline.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Offline = () => {
+    return (
+        <>
+            <span className="status__text">Offline</span>
+        </>
+    )
+};
+
+export default Offline;
